c42147dca699500c585c231e35614a59bfc0d7eb
allal <allalboulkrar97@gmail.com>
2018-12-30 13:20:32 +0100

    style.css and sidemenu.fxml are not used here, please give feedback if needed so i can apply changes quickly

--- client/src/bda1/Main.java
+++ client/src/bda1/Main.java
@@ -1,25 +1,28 @@
 package bda1;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.awt.*;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/Index.fxml"));
+        @Override
+        public void start(Stage primaryStage) throws Exception{
+            Parent root = FXMLLoader.load(getClass().getResource("/Index.fxml"));
+            primaryStage.setTitle("Transport Application GUI");
+            primaryStage.setScene(new Scene(root, 800, 400));
+            primaryStage.show();
 
-        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-        Scene scene = new Scene(root, dimension.getWidth(), dimension.getHeight());
 
-        primaryStage.setTitle("Transport");
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
 
 
